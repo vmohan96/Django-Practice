212f460c81f86ba057a9782d1855c9184d8ceadc
Varun Mohan <vmohan96@gmail.com>
2021-05-17 15:15:37 -0500

    template filters

--- learningtemplates/basicapp/views.py
+++ learningtemplates/basicapp/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'basicapp/index.html')
+    context_dict = {'text':'hello world','number':100}
+    return render(request, 'basicapp/index.html', context_dict)
 
 def other(request):
     return render(request, 'basicapp/other.html')
